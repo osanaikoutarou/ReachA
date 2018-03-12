d1ba90d2cebc6a8e8b515e59364048ae2b956935
長内幸太郎 <osanaikoutarou@osanaikoutarou.local>
2018-03-12 13:48:02 +0900

    channel detail top music

---
+++ ReachA/Screen/Channel/ChannelTop/ChannelTopMusicTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ChannelTopMusicTableViewCell.swift
+//  ReachA
+//
+//  Created by 長内幸太郎 on 2018/03/12.
+//  Copyright © 2018年 osanai. All rights reserved.
+//
+
+import UIKit
+
+class ChannelTopMusicTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
